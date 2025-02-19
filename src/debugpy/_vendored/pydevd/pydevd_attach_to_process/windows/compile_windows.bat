1696e6d7acdbebb3969ec36b31e40e259b64e982
Finn Womack <flan313@gmail.com>
2025-02-18 16:38:04 -0800

    add arm64

--- src/debugpy/_vendored/pydevd/pydevd_attach_to_process/windows/compile_windows.bat
+++ src/debugpy/_vendored/pydevd/pydevd_attach_to_process/windows/compile_windows.bat
@@ -36,6 +36,20 @@ cl /EHsc /Zi /O1 /W3 /Qspectre inject_dll.cpp /link /PROFILE /GUARD:CF /CETCOMPA
 copy inject_dll_amd64.exe ..\inject_dll_amd64.exe /Y
 copy inject_dll_amd64.pdb ..\inject_dll_amd64.pdb /Y
 
+call "%VSDIR%\VC\Auxiliary\Build\vcvarsall.bat" x86_arm64 -vcvars_spectre_libs=spectre
+
+cl -DUNICODE -D_UNICODE /EHsc /Zi /O1 /W3 /LD /MD /Qspectre attach.cpp /link /PROFILE /GUARD:CF /CETCOMPAT /out:attach_arm64.dll
+copy attach_arm64.dll ..\attach_arm64.dll /Y
+copy attach_arm64.pdb ..\attach_arm64.pdb /Y
+
+cl -DUNICODE -D_UNICODE /EHsc /Zi /O1 /W3 /LD /MD /D BITS_64 /Qspectre run_code_on_dllmain.cpp /link /PROFILE /GUARD:CF /CETCOMPAT /out:run_code_on_dllmain_arm64.dll
+copy run_code_on_dllmain_arm64.dll ..\run_code_on_dllmain_arm64.dll /Y
+copy run_code_on_dllmain_arm64.pdb ..\run_code_on_dllmain_arm64.pdb /Y
+
+cl /EHsc /Zi /O1 /W3 /Qspectre inject_dll.cpp /link /PROFILE /GUARD:CF /CETCOMPAT /out:inject_dll_arm64.exe
+copy inject_dll_arm64.exe ..\inject_dll_arm64.exe /Y
+copy inject_dll_arm64.pdb ..\inject_dll_arm64.pdb /Y
+
 del *.exe
 del *.lib
 del *.obj
